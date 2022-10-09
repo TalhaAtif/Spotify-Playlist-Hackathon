24279aad18a6e02a6158d6cab58b8c913b5f50f9
TheVideoDude <97203853+TheVideoDude@users.noreply.github.com>
2022-10-08 23:50:15 -0400

    Update cred.py

--- cred.py
+++ cred.py
@@ -1,3 +1,7 @@
+#Go to https://developer.spotify.com/dashboard/ and create an app to obtain the following info
+
 client_ID = 'client_id'
 client_SECRET = 'client_secret'
+
+#Add redirect URI to your app through app settings
 redirect_URI='http://localhost:9000/'
