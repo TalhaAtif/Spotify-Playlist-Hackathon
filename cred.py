998f9d47edde3f63355eaff0490bd0a63989872c
TheVideoDude <97203853+TheVideoDude@users.noreply.github.com>
2022-10-08 23:48:57 -0400

    Update cred.py

--- cred.py
+++ cred.py
@@ -1,3 +1,3 @@
-client_ID = 'c1fecd981cc946c9826d6d711b8fcb56'
-client_SECRET = 'f84cdc65b8da419db77885d8b47f1322'
+client_ID = 'client_id'
+client_SECRET = 'client_secret'
 redirect_URI='http://localhost:9000/'
